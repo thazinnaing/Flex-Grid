d3cdaf8d9385e43ae7e5c6e0c648febc44dc3326
Thazin <thazinnaingmm@gmail.com>
2023-11-06 14:55:43 +0630

    align self for aligned items

--- src/AlignSelf.jsx
+++ src/AlignSelf.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const AlignSelf = () => {
-  return (
-    <div>
-      
-    </div>
+  return ( // align self for aligned items
+    <div class="flex flex-row justify-between items-start h-60 bg-pink-300 gap-7">
+            <div class="bg-violet-500 w-12 h-7">01</div>
+            <div class="bg-violet-500 w-12 h-7">02</div>
+            <div class="bg-violet-500 w-12 h-7 self-center">03</div>
+            <div class="bg-violet-500 w-12 h-7">04</div>
+            <div class="bg-violet-500 w-12 h-7 self-end">05</div>
+            <div class="bg-violet-500 w-12 h-7">06</div>
+        </div>
   );
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
       <JustifySelf/>
       {/* <AlignContent/> */}
       <AlignItems/>
-      {/* <AlignSelf/> */}
+      <AlignSelf/>
     </div>
   );
 }
