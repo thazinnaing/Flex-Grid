cb216d2a3c1df7a765124fbd2dc049abbbcc4fb3
Thazin <thazinnaingmm@gmail.com>
2023-11-06 13:32:27 +0630

    JustifyContent with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import JustifyContent from './JustifyContent';
+
+
+const App = () => {
+  return (
+    <div>
+      <JustifyContent/>
+      
+    </div>
+  );
+}
+
+export default App;
